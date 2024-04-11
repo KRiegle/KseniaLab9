c924b063939e55e56d8fac89bce035d11510fdc4
kriegle <kriegle@ufl.edu>
2024-04-11 15:14:02 -0400

    -m "comments, removed main() func"

--- kkr_Lab9.py
+++ kkr_Lab9.py
@@ -1,26 +1,24 @@
 #  Ksenia
 
 def encoder(string):
-    nums = [] # creates list
-    for letter in string:
-        nums.append(str(int(letter)+3))
-    return ''.join(nums)
+    nums = []  # creates list to store encoded numbers
+    for letter in string:  # goes through each number in the string
+        nums.append(str(int(letter)+3))  # changes the letter into an int, adds 3, changes back to string
+    return ''.join(nums)  # outputs the joined list of encoded numbers
 
-def menu():
-    print("Menu"
-          "________"
-          "1. Encode"
-          "2. Decode"
-          "3. Quit")
 
 if __name__ == "__main__":
-    while True:
-        menu()
+    while True:  # loops until quit option selected
+        print("Menu"
+              "________"
+              "1. Encode"
+              "2. Decode"
+              "3. Quit")
         selection = input("Please enter an option: ")
-        if selection == "1":
-            code = input("Please enter your password to encode: ")
-            password = encoder(code)
-        elif selection == "2":
-            pass
-        elif selection == "3":
+        if selection == "1":  # Encode
+            code = input("Please enter your password to encode: ")  # input original value
+            password = encoder(code)  # saved encoded value in variable
+        elif selection == "2":  # Decode
+            pass  # insert decode func here
+        elif selection == "3":  # Quit
             break
