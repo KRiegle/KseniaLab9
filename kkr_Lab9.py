ada27dc2145a6414e8e14a0c79927a3d8ccc6e1a
kriegle <kriegle@ufl.edu>
2024-04-11 12:53:05 -0400

    -m "initial"

--- kkr_Lab9.py
+++ kkr_Lab9.py
@@ -1,4 +1,10 @@
 #  Ksenia
 
 def encoder(string):
-    pass
+    nums = []
+    for letter in string:
+        nums.append(str(int(letter)+3))
+    return ''.join(nums)
+
+print(encoder("123456"))
+
