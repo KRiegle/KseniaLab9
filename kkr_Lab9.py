9a4ee65da2146d7a6a6e4d17a737a753d922dff4
kriegle <kriegle@ufl.edu>
2024-04-11 14:33:19 -0400

    -m "added menu and main"

--- kkr_Lab9.py
+++ kkr_Lab9.py
@@ -5,3 +5,22 @@ def encoder(string):
     for letter in string:
         nums.append(str(int(letter)+3))
     return ''.join(nums)
+
+def menu():
+    print("Menu"
+          "________"
+          "1. Encode"
+          "2. Decode"
+          "3. Quit")
+
+if __name__ == "__main__":
+    while True:
+        menu()
+        selection = input("Please enter an option: ")
+        if selection == "1":
+            code = input("Please enter your password to encode: ")
+            password = encoder(code)
+        elif selection == "2":
+            pass
+        elif selection == "3":
+            break
